ba89b65866b21d29281891790441dee7dbd7f972
outrun32 <57797470+outrun32@users.noreply.github.com>
2021-02-22 02:03:31 +0400

    c2

---
+++ Lidar_mesh/Assets/ProjectileLauncher.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLauncher : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody m_proj_Prefab;
+
+    public Rigidbody projectilePrefab { get => m_proj_Prefab; set => m_proj_Prefab = value; }
+
+    public float projectileSpeed = 10f;
+    void Update()
+    {
+        if(m_proj_Prefab == null)
+        {
+            return;
+        }
+        if (Input.touchCount == 0)
+        {
+            return;
+        }
+
+
+        var touch = Input.touches[0];
+        if(touch.phase == TouchPhase.Began)
+        {
+            var ray = GetComponent<Camera>().ScreenPointToRay(touch.position);
+            var projectile = Instantiate(m_proj_Prefab, ray.origin, Quaternion.identity);
+            var rigidbody = projectile.GetComponent<Rigidbody>();
+            rigidbody.velocity = ray.direction * projectileSpeed;
+        }
+    }
+}
